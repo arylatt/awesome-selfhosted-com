d49ae4fbd32240bac9b04e0f579524806c160483
Edward Dickson <ed.kickball@hotmail.com>
2016-07-11 15:19:17 -0700

    Create pull request model

---
+++ app/Models/PullRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PullRequest extends Model
+{
+    public $primaryKey = "pr_id";
+    protected $fillable = ["pr_id", "pr_author", "pr_status", "pr_title", "pr_url", "pr_mergeable", "pr_locked"] ;
+}
