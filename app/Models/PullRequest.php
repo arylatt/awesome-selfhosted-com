a021f378b2adaa6a11fd98dc04f6afb81b8cbfc4
Edward Dickson <ed.kickball@hotmail.com>
2016-07-11 15:30:22 -0700

    Fix Formatting

--- app/Models/PullRequest.php
+++ app/Models/PullRequest.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class PullRequest extends Model
 {
-    public $primaryKey = "pr_id";
-    protected $fillable = ["pr_id", "pr_author", "pr_status", "pr_title", "pr_url", "pr_mergeable", "pr_locked"];
+    public $primaryKey = 'pr_id';
+    protected $fillable = ['pr_id', 'pr_author', 'pr_status', 'pr_title', 'pr_url', 'pr_mergeable', 'pr_locked'];
 }
--- app/Models/Scan.php
+++ app/Models/Scan.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Scan extends Model
 {
-    public $primaryKey = "scan_id";
-    protected $fillable = ["scan_id", "scan_start", "scan_end", "scan_creator", "scan_type", "scan_status"];
+    public $primaryKey = 'scan_id';
+    protected $fillable = ['scan_id', 'scan_start', 'scan_end', 'scan_creator', 'scan_type', 'scan_status'];
 }
