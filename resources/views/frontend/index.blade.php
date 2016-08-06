850c389911a276e6607c776281c5e3ec03170db0
Andrew Rylatt <andrew@rylatt.me>
2016-08-06 19:15:42 +0100

    UI Updates

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,9 +2,60 @@
 
 @section('content')
 	<div class="ui container">
-		<h1 class="ui header">
-			Awesome-Selfhosted
-		</h1>
-		<p>Hey</p>
+		<div class="ui grid">
+			<div class="row">
+				<div class="column">
+					<h1 class="ui header">
+						Awesome-Selfhosted
+					</h1>
+					<p>goals we created a curated list of ___ ____ ___</p>
+				</div>
+			</div>
+			<div class="ui divider"></div>
+			<div class="five column centered row">
+				<div class="column">
+					<img class="ui small centered circular image" src="/img/placeholder.jpg" />
+					<h3 class="ui center aligned header">
+						View Our List
+					</h3>
+				</div>
+				<div class="column">
+					<img class="ui small centered circular image" src="/img/placeholder.jpg" />
+					<h3 class="ui center aligned header">
+						Contribute
+					</h3>
+				</div>
+				<div class="column">
+					<img class="ui small centered circular image" src="/img/placeholder.jpg" />
+					<h3 class="ui center aligned header">
+						Our Sponsors
+					</h3>
+				</div>
+				<div class="column">
+					<img class="ui small centered circular image" src="/img/placeholder.jpg" />
+					<h3 class="ui center aligned header">
+						About / FAQ
+					</h3>
+				</div>
+			</div>
+			<div class="ui divider"></div>
+			<div class="row">
+				<div class="column">
+					<h2 class="ui centered header">News</h2>
+					<div class="ui divided grid">
+						<div class="two column row">
+							<div class="column">
+								<h3 class="ui header">DreamHost Sponsor</h2>
+								<p>_______________------___________</p>
+							</div>
+							<div class="column">
+								<h3 class="ui header">Project of the month</h2>
+								<p>_______________------___________</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 @endsection
