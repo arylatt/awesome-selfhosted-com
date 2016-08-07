528421c00724b53f0d49739c45a32cb9dc3bf6b5
Edward Dickson <ed.kickball@hotmail.com>
2016-08-07 20:18:18 +0000

    Inital grok and scanner push

---
+++ app/Library/Scanner.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Library;
+
+require_once("grok.php");
+$grok = new Grok();
+
+$grokPatterns = array(
+	"githubRepos" => "\[%{DATA}]\(https://github\.com/%{USERNAME:username}/%{USERNAME:repo}\)",
+	"urls" => "\(%{DATA:url}\)"
+);
+
+$today = date("d.m.y");
+$timeThreshold = "P6M";
+$since = $now->sub(new DateInterval($timeThreshold));
+$since = $since->format(DateTime::ATOM);
+
+$fileURL = "https://raw.githubusercontent.com/{$config["gh_repo_owner"]}/{$config["gh_repo_name"]}/master/README.md";
